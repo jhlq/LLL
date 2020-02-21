de78ec8c757876a4062a40b1a99e6fa716d76e7d
Marcus Appelros <marcus.appelros@gmail.com>
2020-02-21 13:40:27 +0100

    ttt

--- ml/cars.js
+++ ml/cars.js
@@ -36,7 +36,7 @@ async function run() {
 	const tensorData = convertToTensor(data);
 	const {inputs, labels} = tensorData;
 		
-	// Train the model  
+	// Train the model	
 	await trainModel(model, inputs, labels);
 	console.log('Done Training');
 	
@@ -61,103 +61,103 @@ function createModel() {
 	return model;
 }
 function convertToTensor(data) {
-  // Wrapping these calculations in a tidy will dispose any 
-  // intermediate tensors.
-  
-  return tf.tidy(() => {
-    // Step 1. Shuffle the data    
-    tf.util.shuffle(data);
+	// Wrapping these calculations in a tidy will dispose any 
+	// intermediate tensors.
+	
+	return tf.tidy(() => {
+		// Step 1. Shuffle the data		
+		tf.util.shuffle(data);
 
-    // Step 2. Convert data to Tensor
-    const inputs = data.map(d => d.horsepower)
-    const labels = data.map(d => d.mpg);
+		// Step 2. Convert data to Tensor
+		const inputs = data.map(d => d.horsepower)
+		const labels = data.map(d => d.mpg);
 
-    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
-    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+		const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+		const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
 
-    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-    const inputMax = inputTensor.max();
-    const inputMin = inputTensor.min();  
-    const labelMax = labelTensor.max();
-    const labelMin = labelTensor.min();
+		//Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+		const inputMax = inputTensor.max();
+		const inputMin = inputTensor.min();	
+		const labelMax = labelTensor.max();
+		const labelMin = labelTensor.min();
 
-    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
-    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+		const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+		const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
 
-    return {
-      inputs: normalizedInputs,
-      labels: normalizedLabels,
-      // Return the min/max bounds so we can use them later.
-      inputMax,
-      inputMin,
-      labelMax,
-      labelMin,
-    }
-  });  
+		return {
+			inputs: normalizedInputs,
+			labels: normalizedLabels,
+			// Return the min/max bounds so we can use them later.
+			inputMax,
+			inputMin,
+			labelMax,
+			labelMin,
+		}
+	});	
 }
 
 async function trainModel(model, inputs, labels) {
-  // Prepare the model for training.  
-  model.compile({
-    optimizer: tf.train.adam(),
-    loss: tf.losses.meanSquaredError,
-    metrics: ['mse'],
-  });
-  
-  const batchSize = 32;
-  const epochs = 50;
-  
-  return await model.fit(inputs, labels, {
-    batchSize,
-    epochs,
-    shuffle: true,
-    callbacks: tfvis.show.fitCallbacks(
-      { name: 'Training Performance' },
-      ['loss', 'mse'], 
-      { height: 200, callbacks: ['onEpochEnd'] }
-    )
-  });
+	// Prepare the model for training.	
+	model.compile({
+		optimizer: tf.train.adam(),
+		loss: tf.losses.meanSquaredError,
+		metrics: ['mse'],
+	});
+	
+	const batchSize = 32;
+	const epochs = 50;
+	
+	return await model.fit(inputs, labels, {
+		batchSize,
+		epochs,
+		shuffle: true,
+		callbacks: tfvis.show.fitCallbacks(
+			{ name: 'Training Performance' },
+			['loss', 'mse'], 
+			{ height: 200, callbacks: ['onEpochEnd'] }
+		)
+	});
 }
 function testModel(model, inputData, normalizationData) {
-  const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
-  
-  // Generate predictions for a uniform range of numbers between 0 and 1;
-  // We un-normalize the data by doing the inverse of the min-max scaling 
-  // that we did earlier.
-  const [xs, preds] = tf.tidy(() => {
-    
-    const xs = tf.linspace(0, 1, 100);      
-    const preds = model.predict(xs.reshape([100, 1]));      
-    
-    const unNormXs = xs
-      .mul(inputMax.sub(inputMin))
-      .add(inputMin);
-    
-    const unNormPreds = preds
-      .mul(labelMax.sub(labelMin))
-      .add(labelMin);
-    
-    // Un-normalize the data
-    return [unNormXs.dataSync(), unNormPreds.dataSync()];
-  });
-  
+	const {inputMax, inputMin, labelMin, labelMax} = normalizationData;	
+	
+	// Generate predictions for a uniform range of numbers between 0 and 1;
+	// We un-normalize the data by doing the inverse of the min-max scaling 
+	// that we did earlier.
+	const [xs, preds] = tf.tidy(() => {
+		
+		const xs = tf.linspace(0, 1, 100);			
+		const preds = model.predict(xs.reshape([100, 1]));			
+		
+		const unNormXs = xs
+			.mul(inputMax.sub(inputMin))
+			.add(inputMin);
+		
+		const unNormPreds = preds
+			.mul(labelMax.sub(labelMin))
+			.add(labelMin);
+		
+		// Un-normalize the data
+		return [unNormXs.dataSync(), unNormPreds.dataSync()];
+	});
+	
  
-  const predictedPoints = Array.from(xs).map((val, i) => {
-    return {x: val, y: preds[i]}
-  });
-  
-  const originalPoints = inputData.map(d => ({
-    x: d.horsepower, y: d.mpg,
-  }));
-  
-  
-  tfvis.render.scatterplot(
-    {name: 'Model Predictions vs Original Data'}, 
-    {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
-    {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
-      height: 300
-    }
-  );
+	const predictedPoints = Array.from(xs).map((val, i) => {
+		return {x: val, y: preds[i]}
+	});
+	
+	const originalPoints = inputData.map(d => ({
+		x: d.horsepower, y: d.mpg,
+	}));
+	
+	
+	tfvis.render.scatterplot(
+		{name: 'Model Predictions vs Original Data'}, 
+		{values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+		{
+			xLabel: 'Horsepower',
+			yLabel: 'MPG',
+			height: 300
+		}
+	);
 }
--- ml/tictactoe.js
+++ ml/tictactoe.js
@@ -1,24 +1,32 @@
 class Board{
-	constructor(){
+	constructor(h){
 		this.m=[[0,0,0],[0,0,0],[0,0,0]];
 		this.p=0;
 		this.h=[];
+		if (h){
+			for (let m of h){
+				this.place(m);
+			}
+		}
 	}
 	place(x,y){
 		if (typeof(y)=='undefined'){
 			y=x%3;
 			x=Math.floor(x/3);
 		}
-		if (this.m[x][y]!=0) return false;
+		if (this.fin || this.m[x][y]!=0) return false;
 		let cp=this.p+1;
 		this.m[x][y]=cp;
-		if ((this.m[x][0]==cp && this.m[x][1]==cp && this.m[x][2]==cp) || (this.m[0][y]==cp && this.m[1][y]==cp && this.m[2][y]==cp) || (this.m[1][1]==cp && (this.m[0][0]==cp && this.m[2][2]==cp) || (this.m[0][2]==cp && this.m[2][0]==cp))){
+		if ((this.m[x][0]==cp && this.m[x][1]==cp && this.m[x][2]==cp) || 
+			(this.m[0][y]==cp && this.m[1][y]==cp && this.m[2][y]==cp) || 
+			(this.m[1][1]==cp && ((this.m[0][0]==cp && this.m[2][2]==cp) || (this.m[0][2]==cp && this.m[2][0]==cp)))){
 			this.winner=cp;
 			this.fin=true;
 		}
 		this.p=cp%2;
-		this.h.push(x+y);
+		this.h.push(x*3+y);
 		if (this.h.length>8) this.fin=true;
+		return true;
 	}
 	print(){
 		let syms=[' ','O','X'];
@@ -27,19 +35,154 @@ class Board{
 		console.log(syms[this.m[1][0]]+"|"+syms[this.m[1][1]]+"|"+syms[this.m[1][2]]);
 		//console.log("_____");
 		console.log(syms[this.m[2][0]]+"|"+syms[this.m[2][1]]+"|"+syms[this.m[2][2]]);
-		console.log(this.winner);
+		console.log("O: 1, X: 2, Winner: "+this.winner);
 	}
 	copy(){
 		let nb=new Board;
 		nb.m=JSON.parse(JSON.stringify(this.m));
+		nb.p=this.p;
+		nb.h=JSON.parse(JSON.stringify(this.h));
 		return nb;
 	}
 }
 class AI{
 	constructor(b){
-		this.b=b;
+		this.b=b||new Board();
+		this.explore=true;
+		this.usemodel=true;
+		this.model=this.createModel();
 	}
-	evaluate(){
-		return Math.random()-0.5;
+	vectorize(b){
+		b=b||this.b;
+		let v=[];
+		for (let i=0;i<3;i++){
+			for (let j=0;j<3;j++){
+				let temp=[0,0];
+				let temp2=b.m[i][j];
+				if (temp2>0) temp[temp2-1]=1;
+				v.push(temp[0]);v.push(temp[1]);
+			}
+		}
+		return v;
+	}
+	createModel() {
+		const model = tf.sequential(); 
+		let bv=this.vectorize(this.b);
+		model.add(tf.layers.dense({inputShape: [bv.length], units: 10, useBias: true}));
+		model.add(tf.layers.dense({units: 500, activation: 'sigmoid', useBias: true}));
+		model.add(tf.layers.dense({units: 1, activation: 'sigmoid', useBias: true}));
+		return model;
+	}
+	runModel(bv){
+		bv=bv||this.vectorize(this.b);
+		if (bv.m) bv=this.vectorize(bv);
+		let bvt=tf.tensor2d(bv, [1, bv.length]);
+		return this.model.predict(bvt).dataSync()[0];
+	}
+	async trainModel(inputs, labels) {
+		inputs=tf.tensor2d(inputs, [inputs.length, inputs[0].length]);
+		labels=tf.tensor2d(labels, [labels.length, 1]);
+		this.model.compile({
+			optimizer: tf.train.adam(),
+			loss: tf.losses.meanSquaredError,
+			metrics: ['mse'],
+		});
+		
+		const batchSize = 32;
+		const epochs = 50;
+		
+		return await this.model.fit(inputs, labels, {
+			batchSize,
+			epochs,
+			shuffle: true,
+			callbacks: tfvis.show.fitCallbacks(
+				{ name: 'Training Performance' },
+				['loss', 'mse'], 
+				{ height: 200, callbacks: ['onEpochEnd'] }
+			)
+		});
+	}
+	evaluate(move,y){
+		if (y) move+=y;
+		let bc=this.b.copy();
+		let placed=bc.place(move);
+		if (!placed) return 0;
+		if (bc.winner==this.b.p+1) return 1;
+		if (!this.usemodel) return 0.5;
+		let ev=this.runModel(bc);
+		if (this.b.p==1) ev=1-ev;
+		return ev;
+	}
+	evaluations(){
+		let ev=[];
+		for (let m=0;m<9;m++){
+			ev.push(this.evaluate(m));
+		}
+		return ev;
+	}
+	select(mps){
+		if (this.explore){
+			let sum=mps.reduce((t,p)=>t+=p);
+			let r=sum*Math.random();
+			let rs=0;
+			for (let mi=0;mi<mps.length;mi++){
+				rs+=mps[mi];
+				if (rs>r) return mi;
+			}
+		} else {
+			let m=Math.max(...mps);
+			for (let mi=0;mi<mps.length;mi++){
+				if (mps[mi]==m) return mi;
+			}
+		}
+	}
+	place(n){
+		if (!n) n=1;
+		for (let ni=0;ni<n;ni++){
+			this.b.place(this.select(this.evaluations()));
+			if (this.b.fin) break;
+		}
+	}
+	play(n){
+		if (!n) n=1;
+		let r=[];
+		for (let ni=0;ni<n;ni++){
+			let ai=new AI(this.b.copy());
+			ai.place(9);
+			let t=ai.b.winner?(ai.b.winner-1):0.5;
+			r.push([ai.b.h,t]);
+		}
+		return r;
+	}
+	makedata(r){
+		let inp=[];
+		let lab=[];
+		for (let ra of r){
+			let h=ra[0];
+			let hn=h.length;
+			let target=ra[1];
+			for (let hi=0;hi<hn;hi++){
+				let tb=new Board(h);
+				inp.push(this.vectorize(tb));
+				lab.push(target);
+				h.pop();
+			}
+		}
+		return [inp,lab];
 	}
 }
+function time(){
+	let ai=new AI();
+	let d=new Date();
+	let s=d.getTime();
+	ai.play(100);
+	d=new Date();
+	console.log(d.getTime()-s);
+	ai.usemodel=false;
+	d=new Date();
+	s=d.getTime();
+	ai.play(100);
+	d=new Date();
+	console.log(d.getTime()-s);
+};
+//ai=new AI;ai.trainModel(...ai.makedata(ai.play(10)))
